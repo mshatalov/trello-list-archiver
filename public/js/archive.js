8c4408910a8c889fcdd754ce6bf10b286d92db55
Mikhail Shatalov <m@shatalov.us>
2018-12-21 13:10:35 -0600

    Fixed lists lookup not working if the glitch app has to warm up

--- public/js/archive.js
+++ public/js/archive.js
@@ -27,69 +27,66 @@ let archiveSelectedLists = function (close) {
   return requests;
 };
 
-t.render(() => {
-  // helpers
-  let archiveSubmit = window.archiveSubmit;
+let archiveSubmit = window.archiveSubmit;
 
-  let showList = function () {
-    archiveSubmit.innerHTML = archiveAction;
-    window.notFound.setAttribute('hidden', '');
-    window.archive.removeAttribute('hidden');
-  };
+let showList = function () {
+  archiveSubmit.innerHTML = archiveAction;
+  window.notFound.setAttribute('hidden', '');
+  window.archive.removeAttribute('hidden');
+};
 
-  let showNotFound = function () {
-    window.archive.setAttribute('hidden', '');
-    window.notFound.removeAttribute('hidden');
-  };
+let showNotFound = function () {
+  window.archive.setAttribute('hidden', '');
+  window.notFound.removeAttribute('hidden');
+};
 
-  let getAction = function () {
-    return archiveSubmit.innerHTML === archiveAction;
-  };
+let getAction = function () {
+  return archiveSubmit.innerHTML === archiveAction;
+};
 
-  let toggleAction = function () {
-    archiveSubmit.innerHTML = getAction() ? undoAction : archiveAction;
-  };
+let toggleAction = function () {
+  archiveSubmit.innerHTML = getAction() ? undoAction : archiveAction;
+};
 
-  // search action
-  window.search.addEventListener('submit', (event) => {
-    event.preventDefault();
+// search action
+window.search.addEventListener('submit', (event) => {
+  event.preventDefault();
 
-    let q = window.search.listName.value.trim();
-    return t.lists('all')
-      .then((lists) => {
-        let uiList = clear();
+  let q = window.search.listName.value.trim();
+  return t.lists('all')
+    .then((lists) => {
+      let uiList = clear();
 
-        let re = new RegExp(q);
-        let found = false;
-        for (let l in lists) {
-          if (re.test(lists[l].name)) {
-            uiList.insertAdjacentHTML(
-              'beforeend',
-              `<label for="i${lists[l].id}"><input type="checkbox" id="i${lists[l].id}" class="list-item" value="${lists[l].id}" checked />${lists[l].name}</label>`
-            );
-            found = true;
-          }
+      let re = new RegExp(q);
+      let found = false;
+      for (let l in lists) {
+        if (re.test(lists[l].name)) {
+          uiList.insertAdjacentHTML(
+            'beforeend',
+            `<label for="i${lists[l].id}"><input type="checkbox" id="i${lists[l].id}" class="list-item" value="${lists[l].id}" checked />${lists[l].name}</label>`
+          );
+          found = true;
         }
-        return found;
-      })
-      .then((any) => any ? showList() : showNotFound())
-      .then(() => t.sizeTo('#content'));
-  });
-
-  // archive/undo action
-  window.archive.addEventListener('submit', (event) => {
-    event.preventDefault();
-    archiveSubmit.setAttribute('disabled', '');
-
-    Promise.all(archiveSelectedLists(getAction())).then(
-      toggleAction,
-      () => t.popup({
-        title: 'Oops',
-        url: './error.html',
-        height: 70
-      })
-    ).finally(() => { archiveSubmit.removeAttribute('disabled'); });
-  });
-
-  t.sizeTo('#content').done();
+      }
+      return found;
+    })
+    .then((any) => any ? showList() : showNotFound())
+    .then(() => t.sizeTo('#content'));
 });
+
+// archive/undo action
+window.archive.addEventListener('submit', (event) => {
+  event.preventDefault();
+  archiveSubmit.setAttribute('disabled', '');
+
+  Promise.all(archiveSelectedLists(getAction())).then(
+    toggleAction,
+    () => t.popup({
+      title: 'Oops',
+      url: './error.html',
+      height: 70
+    })
+  ).finally(() => { archiveSubmit.removeAttribute('disabled'); });
+});
+
+t.sizeTo('#content').done();
