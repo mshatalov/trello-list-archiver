742383bab6f7abc19aaa50fc4553f11f54aaa541
Mikhail Shatalov <m@shatalov.us>
2018-12-26 15:24:20 -0600

    Minor code cleanup and optimizations

--- js/client.js
+++ js/client.js
@@ -1,42 +1,32 @@
 'use strict';
 
-let promptAuthorization = (t) => {
-  if (window.appKey) {
-    return t.popup({
-      title: 'Authorize',
-      url: './authorize.html',
-      height: 140
-    });
-  } else {
-    console.log('🙈 Looks like you need to add your API key to the project!');
-  }
+const authPopup = {
+  title: 'Authorize',
+  url: './authorize.html',
+  height: 140
+};
+
+const archivePopup = {
+  title: 'Archive Lists',
+  url: './archive.html',
+  height: 119
 };
 
 TrelloPowerUp.initialize({
-  'board-buttons': (t, options) => {
+  'board-buttons': (t, opts) => {
     return [{
       icon: {
         dark: document.getElementById('white-icon').href,
         light: document.getElementById('black-icon').href
       },
       text: 'Archive Lists',
-      callback: (t) => {
-        return t.getRestApi().isAuthorized().then((authorized) => {
-          if (authorized) {
-            return t.popup({
-              title: 'Archive Lists',
-              url: 'archive.html',
-              height: 119
-            });
-          } else {
-            return promptAuthorization(t);
-          }
-        });
-      }
+      callback: (t) => t.getRestApi()
+        .isAuthorized()
+        .then((auth) => t.popup(auth ? archivePopup : authPopup))
     }];
   },
   'authorization-status': (t) => t.getRestApi().isAuthorized().then((authorized) => ({ authorized })),
-  'show-authorization': promptAuthorization
+  'show-authorization': (t) => t.popup(authPopup)
 }, {
   appKey: window.appKey,
   appName: window.appName
